a22344bb3afdb1b0e958d00964e2b1f383308540
WilKen3 <wktanaka.13@gmail.com>
2022-01-22 19:47:14 +0900

    arm motor prototype 1

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -1,6 +1,7 @@
 package frc.robot;
 
 import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 /**import com.ctre.phoenix.motorcontrol.*;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;**/
 import com.ctre.phoenix.motorcontrol.can.VictorSPX;
@@ -13,7 +14,7 @@ import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 
 
 public class Robot extends TimedRobot {
-WPI_TalonSRX dRF, dLF;
+WPI_TalonSRX dRF, dLF, armMotor;
 VictorSPX dRB, dLB, intakeMotor; 
 DifferentialDrive drive;
 XboxController driveController;
@@ -33,6 +34,7 @@ XboxController driveController;
     driveController = new XboxController(0);
     drive = new DifferentialDrive(dLF, dRF);
     intakeMotor = new VictorSPX(14);
+    armMotor = new WPI_TalonSRX(3);
   }
   @Override
   public void robotPeriodic(){
@@ -62,21 +64,32 @@ XboxController driveController;
 
   @Override
   public void teleopPeriodic() {
-    double intakeMotorInputL = driveController.getLeftTriggerAxis();
-    double intakeMotorInputR = driveController.getRightTriggerAxis();
-    
+  
+    boolean armMotorInput = driveController.getBButton();
+    if(armMotorInput == true){
+      armMotor.set(ControlMode.PercentOutput,-0.5);
+    } else {
+      armMotor.set(ControlMode.PercentOutput,-0.2);
+    }
+
+
+
     double forwardSpeed =driveController.getLeftY();
     double rotationSpeed = driveController.getRightX();
     drive.arcadeDrive(-forwardSpeed,rotationSpeed);
+
+    double intakeMotorInputL = driveController.getLeftTriggerAxis();
+    double intakeMotorInputR = driveController.getRightTriggerAxis();
     if(intakeMotorInputL>0.03 && intakeMotorInputR>0.03){
-      intakeMotor.set(ControlMode.PercentOutput,intakeMotorInputL);
+      intakeMotor.set(ControlMode.PercentOutput,-intakeMotorInputL*0.1);
     } else if(intakeMotorInputL>0.03 && intakeMotorInputR<0.03){
-      intakeMotor.set(ControlMode.PercentOutput,intakeMotorInputL);
+      intakeMotor.set(ControlMode.PercentOutput,-intakeMotorInputL*0.1);
     } else if(intakeMotorInputL<0.03 && intakeMotorInputR>0.03){
-      intakeMotor.set(ControlMode.PercentOutput,-intakeMotorInputR);
+      intakeMotor.set(ControlMode.PercentOutput,intakeMotorInputR*0.1);
     } else{
       intakeMotor.set(ControlMode.PercentOutput,0);
     }
+
     
     
     }
